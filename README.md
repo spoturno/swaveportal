717430c5d0508ffddfb8f9faa89f718f9b2743ed
spoturno <spoturno16@gmail.com>
2022-01-25 20:15:02 -0300

    update README.md and comments in App.js

--- README.md
+++ README.md
@@ -4,9 +4,21 @@
 
 This is my personal wave portal, the name stands for sporturno wave portal.
 
-# Overview
+### **[Smart Contract](https://rinkeby.etherscan.io/address/0xafDa0d14Aa2AfF61292704bD442D0FB083eAC25C)**
 
-In this webiste anyone on the internet can send me a wave (message) and have that data stored on the blockchain through an Ethereum smart contract. This project was build using HardHat framework and the smart contract was deployed to Rinkeby test network. When a user leaves a message, they have a 50% chance of being rewarded with an amount of ETH. There is also an anti-bot system to prevent spamming where the same account must wait at least 30 (testing purposes)
+## Overview
+
+In this webiste anyone on the internet can send me a wave (message) and have that data stored on the blockchain through an Ethereum smart contract. This project was build using HardHat framework and the smart contract was deployed to Rinkeby test network. When a user leaves a message, they have a 50% chance of being rewarded with an amount of ETH. There is also an anti-bot system to prevent spamming where the same account must wait at least 30 (testing purposes).
+
+## Technologies used
+
+- Frontend hosted with [Heroku](https://heroku.com)
+- Solidity project with [Hardhat](https://hardhat.org/) framework
+- Deployed with [Rinkeby](https://rinkeby.io/#stats) test network
+- Contract address: **0xafDa0d14Aa2AfF61292704bD442D0FB083eAC25C**, also check this link: [rinkeby.etherscan](https://rinkeby.etherscan.io/address/0xafDa0d14Aa2AfF61292704bD442D0FB083eAC25C)
+- Users must connect using [Metamask](https://metamask.io)
+
+---
 
 ## Basic Sample Hardhat Project
 
--- src/App.js
+++ src/App.js
@@ -5,19 +5,32 @@ import { ethers } from "ethers"
 
 
 const App = () => {
-  //state variable to store user's public waller
+
+  // state varialbe to store user's public wallet
   const [currentAccount, setCurrentAccount] = useState("");
+
+  // state variable to store user's input - wave message
   const [message, setMessage] = useState("");
+
+  // state variable to store all past waves 
   const [allWaves, setAllWaves] = useState([]);
+
+  // state variable to store wave count
   const [waveCount, setWaveCount] = useState(0);
+
+  // state variable to handle change in wave count
   const [state, chageState] = useState(false);
 
+  // contract address and contract ABI after deploy to rinkeby test network
   const contractAddress = "0xafDa0d14Aa2AfF61292704bD442D0FB083eAC25C";
   const contractABI = abi.abi;
 
+
   const checkIfWalletIsConnected = async () => {
     try{
+
       const { ethereum } = window;
+
       if(!ethereum)
         console.log("Make sure you have Metamask!");
       else console.log("We have the ethereum object", ethereum);
@@ -40,13 +53,18 @@ const App = () => {
   const connectWallet = async () => {
     try{
       const { ethereum } = window;
+
       if(!ethereum){
         alert("Get Metamask!");
         return;
       }
-      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
 
+      // Gets all the accounts from Metamask browser
+      const accounts = await ethereum.request({ method: "eth_requestAccounts"});
+     
       console.log("Connected", accounts[0]);
+
+      // change currentAccount state to first account
       setCurrentAccount(accounts[0]);
 
     } catch(error){
@@ -57,33 +75,37 @@ const App = () => {
  const wave = async () => {
     try {
       const { ethereum } = window;
-
       if (ethereum) {
+        // A Web3Provider wraps a standard Web3 provider, which is
+        // what MetaMask injects as window.ethereum into each page
         const provider = new ethers.providers.Web3Provider(ethereum);
+
+        // The MetaMask plugin also allows signing transactions to
+        // send ether and pay to change state within the blockchain.
+        // For this, you need the account signer...
         const signer = provider.getSigner();
+
+        // The contract object
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
 
+        // Calls the getAllWaves method from smart contract
         let count = await wavePortalContract.getWaveCount();
+
         console.log("Retrieved total wave count...", count.toNumber());
 
-        /*
-        * Execute the actual wave from your smart contract
-        */
+        //Execute the actual wave from your smart contract
         const waveTxn = await wavePortalContract.wave(message, { gasLimit: 300000 });
-        console.log("Mining...", waveTxn.hash);
+        console.log("Mining ...", waveTxn.hash);
 
         await waveTxn.wait();
         console.log("Mined -- ", waveTxn.hash);
 
+        // get the wave count after block is mined,
+        // transaction valid -> wave successfully send
         count = await wavePortalContract.getWaveCount();
-        //await count.wait();
-        setWaveCount(count.toString());
-
-        
         setWaveCount(count.toNumber());
 
         console.log("Retrieved total wave count...", waveCount);
-
 
       } else {
         console.log("Ethereum object doesn't exist!");
@@ -93,8 +115,13 @@ const App = () => {
     }
   } 
 
+  // check for invalid input, calls wave and empties the field
   const handleSubmit = (evt) => {
+
+    // If the event does not get explicitly handled,
+    // its default action should not be taken as it normally would be.
     evt.preventDefault();
+
     if(message == ""){
       alert("This field must be filled out");
       return false;
@@ -111,10 +138,12 @@ const App = () => {
         const signer = provider.getSigner();
         const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
 
-        //call the getAllWaves method from smart contract
+        // Calls the getAllWaves method from smart contract
         const waves = await wavePortalContract.getAllWaves();
 
         let wavesCleaned = [];
+
+        // stores waves info from smart contract like object
         waves.forEach(wave => {
           wavesCleaned.push({
             address: wave.waver,
@@ -122,22 +151,25 @@ const App = () => {
             message: wave.message
           }) 
         });
+
+        // updates allWaves state variable
         setAllWaves(wavesCleaned);
 
+        // Listens to smart contract event 
         wavePortalContract.on("NewWave", (from, timestamp, message) => {
+
           console.log("NewWave", from, timestamp, message);
 
+          // updates previous allWaves state by adding new wave
           setAllWaves(prevState => [...prevState, {
             address: from,
             timestamp: new Date(timestamp * 1000),
             message: message
           }]);
 
-          //setWaveCount(prevState => prevState + 1);
+          // update state so that waveCount updates in updateWaveCount
           chageState(!state); 
-
         });
-
       }else{
         console.log("Ethereum object doesn't exist!");
       }
@@ -146,9 +178,7 @@ const App = () => {
    }
   }
 
-
   const updateWaveCount = async () => {
-
       const { ethereum } = window;
       if(ethereum){
         const provider = new ethers.providers.Web3Provider(ethereum);
@@ -157,24 +187,21 @@ const App = () => {
 
         const count = await wavePortalContract.getWaveCount();
         setWaveCount(count.toNumber());
-
       }
   }
 
-
-  
-  // listen for emitter events
+  // Listens for emitter events
   useEffect(() => {
     checkIfWalletIsConnected();
     getAllWaves();
   }, [])
 
+  // Listens for changes in state,
+  // if changes then waveCount is updated in real time
   useEffect(() => {
     updateWaveCount();
   }, [state])
 
-
-  
 
   return(
     <div className="mainContainer">
@@ -188,6 +215,7 @@ const App = () => {
           This is my personal wave portal. Connect your Ethereum wallet and wave at me!
         </div>
 
+        {/* message input section */}
         <form className="waveForm" onSubmit={handleSubmit}>
           <label>
             <input className="waveInput" type="text" value={message} onChange={e => setMessage(e.target.value)} />
@@ -205,12 +233,14 @@ const App = () => {
           </button>
         )}
 
+        {/* title and total waves */}
         <div className="waves">
           <div className="waves-title">
             <h3>Lasts waves</h3>
             <h3>Total: {waveCount}</h3>
           </div>
 
+          {/* show each wave stored in with smart contract */}
           {allWaves.map((wave, index) => {
             return (
               <div key={index} className="waveObject">
